19834174ea010f6f6a678cd654a03db341b9a24a
4c656f <vlevo.yovlyo@yandex.ru>
2024-09-23 18:33:47 +0500

    fix: rebuild on push

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ else
 fi
 
 # Build and start the new services
-docker-compose up -d --force-recreate --no-deps
+docker-compose up -d --force-recreate --no-deps --build
 
 echo "Update completed successfully"
